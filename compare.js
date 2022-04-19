ff215d46a30c8580a9d44a90d566bced1fe36709
Burhanuddin5275 <103917627+Burhanuddin5275@users.noreply.github.com>
2022-04-19 23:09:22 +0500

    Add files via upload

---
+++ compare.js
@@ -0,0 +1,191 @@
+ 
+function selectProduct1()
+{
+
+    document.getElementById("product1").style.opacity = 1;
+
+    var product = document.getElementById("first").value;
+    if(product=="val1")
+    {
+        document.getElementById("image").src="./assets/image/Blood_Sun-kit-removebg-preview.png";
+        document.getElementById("brand").innerHTML="Brand : Kits";
+        document.getElementById("model").innerHTML="Model : Blood Sun";
+        document.getElementById("features").innerHTML="Lense: 	Prescription lenses included";
+        document.getElementById("spec").innerHTML="Material: Metal";
+        
+    }
+    else if(product=="val2")
+    {
+        document.getElementById("image").src="./assets/image/Bruchay_Sun-kit-removebg-preview.png" ;
+        document.getElementById("brand").innerHTML="Brand : Kits";
+        document.getElementById("model").innerHTML="Model : Bruchay Sun";
+        document.getElementById("features").innerHTML="Lense: 	Prescription lenses included";
+        document.getElementById("spec").innerHTML="Material:  Acetate";
+    }
+    else if(product=="val3")
+    {
+        document.getElementById("image").src="./assets/image/Mitchell_Sun-kit-removebg-preview.png" ;
+        document.getElementById("brand").innerHTML="Brand : Kits";
+        document.getElementById("model").innerHTML="Model : Mitchell Sun";
+        document.getElementById("features").innerHTML="Lense: 	Prescription lenses included";
+        document.getElementById("spec").innerHTML="Material:  Metal";
+        
+    }
+    else if(product=="val4")
+    {
+        document.getElementById("image").src="./assets/image/Blackcomb-removebg-preview.png" ;
+        document.getElementById("brand").innerHTML="Brand : Kits";
+        document.getElementById("model").innerHTML="Model : Blackcomb";
+        document.getElementById("features").innerHTML="Lense: 	Prescription lenses included";
+        document.getElementById("spec").innerHTML="Material:  	Eco Acetate";
+        
+    }
+    else if(product=="val5")
+    {
+        document.getElementById("image").src="./assets/image/Discovery-kit-removebg-preview.png" ;
+        document.getElementById("brand").innerHTML="Brand : Kits";
+        document.getElementById("model").innerHTML="Model : Discovery";
+        document.getElementById("features").innerHTML="Lense: 	Prescription lenses included";
+        document.getElementById("spec").innerHTML="Material:   Acetate";
+        
+    }
+     else if(product=="val6")
+    {
+        document.getElementById("image").src="./assets/image/Dundar-kit-removebg-preview.png" ;
+        document.getElementById("brand").innerHTML="Brand : Kits";
+        document.getElementById("model").innerHTML="Model : Dunbar";
+        document.getElementById("features").innerHTML="Lense: 	Prescription lenses included";
+        document.getElementById("spec").innerHTML="Material:  Metal";
+        
+    }
+     else if(product=="val7")
+    {
+        document.getElementById("image").src="https://www.opticontacts.com/cart/images/items/focus_night_and_day.png" ;
+        document.getElementById("brand").innerHTML="Brand : Air Optix";
+        document.getElementById("model").innerHTML="Model : Focus Night and Day";
+        document.getElementById("features").innerHTML="Material: 24% water, 76% lotrafilcon A polymer";
+        document.getElementById("spec").innerHTML="Replacement: 1 month extended wear<br>";
+        
+    }
+     else if(product=="val8")
+    {
+        document.getElementById("image").src="https://www.opticontacts.com/cart/images/items/Air%20Optix%20Aqua%20Multifocal_1.jpg" ;
+        document.getElementById("brand").innerHTML="Brand : Air Optix";
+        document.getElementById("model").innerHTML="Model : Aqua Multifocal";
+        document.getElementById("features").innerHTML="Material: 67% lotrafilcon B polymer, 33% H2O<br>";
+        document.getElementById("spec").innerHTML="Replacement:  Monthly<br>";
+        
+    }
+     else if(product=="val9")
+    {
+        document.getElementById("image").src="https://www.opticontacts.com/cart/images/items/Air%20Optix%20Colors_1.jpg" ;
+        document.getElementById("brand").innerHTML="Brand : Air Optix";
+        document.getElementById("model").innerHTML="Model : Air Optix Colors";
+        document.getElementById("features").innerHTML="Material: Lotrafilcon B<br>";
+        document.getElementById("spec").innerHTML="Replacement: 1 month daily wear<br>";
+        
+        
+    }
+    else 
+    {
+        document.getElementById("product1").style.opacity = 0;
+    }
+}
+
+
+function selectProduct2()
+{
+
+    document.getElementById("product2").style.opacity = 1;
+
+    var product = document.getElementById("second").value;
+    if(product=="valu1")
+    {
+        document.getElementById("image2").src="./assets/image/Ray-Ban_RB1969-removebg-preview.png" ;
+        document.getElementById("brand1").innerHTML="Brand : Ray-Ban";
+        document.getElementById("model2").innerHTML="Model : RB1969";
+        document.getElementById("features2").innerHTML="Lenses:	Prescription lenses not available";
+        document.getElementById("spec2").innerHTML="Material: metal";
+        
+    }
+    else if(product=="valu2")
+    {
+        document.getElementById("image2").src="./assets/image/Ray-Ban_RB3016-removebg-preview.png" ;
+        document.getElementById("brand1").innerHTML="Brand : Ray-Ban";
+        document.getElementById("model2").innerHTML="Model : RB3016";
+        document.getElementById("features2").innerHTML="Lenses:	Prescription lenses not available";
+        document.getElementById("spec2").innerHTML="Material: metal"
+    }
+    else if(product=="valu3")
+    {
+        document.getElementById("image2").src="./assets/image/Ray-Ban_RB3025-removebg-preview.png" ;
+        document.getElementById("brand1").innerHTML="Brand : Ray-Ban";
+        document.getElementById("model2").innerHTML="Model : RB3025";
+        document.getElementById("features2").innerHTML="Lenses:	Prescription lenses not available";
+        document.getElementById("spec2").innerHTML="Material: metal"
+        
+    }
+    else if(product=="valu4")
+    {
+        document.getElementById("image2").src="./assets/image/Ray-Ban_RX1972-removebg-preview.png" ;
+        document.getElementById("brand1").innerHTML="Brand : Ray-Ban";
+        document.getElementById("model2").innerHTML="Model : RX1972";
+        document.getElementById("features2").innerHTML="Lenses:	Prescription lenses not available";
+        document.getElementById("spec2").innerHTML="Material: metal"
+    }
+    else if(product=="valu5")
+    {
+        document.getElementById("image2").src="./assets/image/Ray-Ban_RX4246V-removebg-preview.png" ;
+        document.getElementById("brand1").innerHTML="Brand : Ray-Ban";
+        document.getElementById("model2").innerHTML="Model : RX4246V";
+        document.getElementById("features2").innerHTML="Lenses:	Prescription lenses not available";
+        document.getElementById("spec2").innerHTML="Material: metal"
+        
+    }
+     else if(product=="valu6")
+    {
+        document.getElementById("image2").src="./assets/image/Ray-Ban_RX6448-removebg-preview.png" ;
+        document.getElementById("brand1").innerHTML="Brand : Ray-Ban";
+        document.getElementById("model2").innerHTML="Model : RX6448";
+        document.getElementById("features2").innerHTML="Lenses:	Prescription lenses not available";
+        document.getElementById("spec2").innerHTML="Material: metal"
+        
+    }
+     else if(product=="valu7")
+    {
+        document.getElementById("image2").src="https://www.opticontacts.com/cart/images/items/Air%20Optix%20Night%20and%20Day%20Aqua_1.jpg" ;
+        document.getElementById("brand1").innerHTML="Brand : Air Optix";
+        document.getElementById("model2").innerHTML="Model : Night and Day Aqua";
+        document.getElementById("features2").innerHTML="Material: 	76% lotrafilcon A polymer, 24% H2O<br>";
+        document.getElementById("spec2").innerHTML="Replacement:Monthly<br>";
+        
+    }
+     else if(product=="valu8")
+    {
+        document.getElementById("image2").src="https://www.opticontacts.com/cart/images/items/Air%20Optix%20plus%20HydraGlyde%20Multifocal_1.jpg" ;
+        document.getElementById("brand1").innerHTML="Brand : Air Optix";
+        document.getElementById("model2").innerHTML="Model : plus HydraGlyde Multifocal";
+        document.getElementById("features2").innerHTML="Material: 67% lotrafilcon B<br>";
+        document.getElementById("spec2").innerHTML="Replacement:  Monthly<br>";
+        
+        
+    }
+     else if(product=="valu9")
+    {
+        document.getElementById("image2").src="https://www.opticontacts.com/cart/images/items/Air%20Optix%20Colors%202%20Pack_1.jpg" ;
+        document.getElementById("brand1").innerHTML="Brand : Air Optix";
+        document.getElementById("model2").innerHTML="Model : Air Optix Colors 2Pack";
+        document.getElementById("features2").innerHTML="Material: 	Lotrafilcon B<br>";
+        document.getElementById("spec2").innerHTML="Replacement: 1 month daily wear<br>";
+        
+    }
+    else 
+    {
+        document.getElementById("product2").style.opacity = 0;
+    }
+}
+
+
+
+
+
